09db69ba8f3e8f5c950cd26c1940d136b4a9ac00
Sanjaee <afrizaahmad18@gmail.com>
2023-11-27 16:41:34 +0700

    deploy

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
+// server.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const {
-  generateMeta,
-  generateImage,
-} = require("./controllers/openaiControllers");
+const OpenAI = require("openai");
 
 const app = express();
 
-app.use(cors()); // Tambahkan ini untuk mengaktifkan CORS
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+const openai = new OpenAI({
+  apiKey: "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",
+});
+
+const generateMeta = async (req, res) => {
+  const { title } = req.body;
+
+  const description = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `isi ${title}`,
+      },
+    ],
+  });
+
+  const tags = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `sebutkan tags dari  ${title}`,
+      },
+    ],
+  });
+
+  res.status(200).json({
+    description: description.choices[0].message,
+    tags: tags.choices[0].message,
+  });
+};
+
+const generateImage = async (req, res) => {
+  const image = await openai.images.generate({
+    model: "dall-e-3",
+    prompt: req.body.prompt,
+  });
+
+  res.status(200).json({
+    image: image.data,
+  });
+};
 
 app.post("/openai/meta", generateMeta);
 app.post("/openai/image", generateImage);
--- config/openaiConfig.js
+++
@@ -1,8 +0,0 @@
-// openaiConfig.js
-const OpenAI = require("openai");
-
-const openai = new OpenAI({
-  apiKey: "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",
-});
-
-module.exports = openai;
